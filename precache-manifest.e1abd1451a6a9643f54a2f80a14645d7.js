08312d4d1b0f85df74102f1b20cd447861d14315
Matthew <matthewmacraebovell@outlook.com>
2020-08-01 15:43:11 -0400

    Updates

---
+++ precache-manifest.e1abd1451a6a9643f54a2f80a14645d7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "63ad2d67743025a9e5bcc00d99c764ad",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a2ae00c8709914948cd6",
+    "url": "/static/css/2.ac1ab58b.chunk.css"
+  },
+  {
+    "revision": "144d3ce233670d7e97ba",
+    "url": "/static/css/main.e44118e8.chunk.css"
+  },
+  {
+    "revision": "a2ae00c8709914948cd6",
+    "url": "/static/js/2.e19bca71.chunk.js"
+  },
+  {
+    "revision": "a73511fdd0e79fea52c5f42ad1574633",
+    "url": "/static/js/2.e19bca71.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "144d3ce233670d7e97ba",
+    "url": "/static/js/main.705119e1.chunk.js"
+  },
+  {
+    "revision": "1245fb1a597d78d38e1b",
+    "url": "/static/js/runtime-main.d103cc1e.js"
+  }
+]);
